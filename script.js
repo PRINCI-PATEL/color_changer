d1b29eb66f8fd3c04ea2de4f47d4196f6b8fe221
PRINCI-PATEL <principatel6307@gmail.com>
2025-07-14 15:57:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let ans =document.querySelector(".first");
+ans.addEventListener ("click",function(){
+    document.querySelector("body").style.backgroundColor = "aqua";
+}) ;                                                                                                                                   
+let wer =document.querySelector(".second");
+wer.addEventListener ("click",function(){
+    document.querySelector("body").style.backgroundColor = "red";
+}) ;              
+let tic =document.querySelector(".third");
+tic.addEventListener ("click",function(){
+    document.querySelector("body").style.backgroundColor = "chocolate";
+}) ;              
+let pow =document.querySelector(".fourth");
+pow.addEventListener ("click",function(){
+    document.querySelector("body").style.backgroundColor = "blue";
+}) ;              
